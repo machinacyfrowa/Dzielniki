50e763d59220c4322739620799ac656c0fe713b8
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-04-08 12:18:14 +0200

    zapis w tablicy

--- Dzielniki/Program.cs
+++ Dzielniki/Program.cs
@@ -2,16 +2,25 @@
 Console.WriteLine("Podaj początkową liczbę: "); //zapytaj użytkownika o liczbę
 
 liczba = int.Parse(Console.ReadLine() ?? "0"); //przeczytaj liczbę od użytkownika
-                                                // jeśli readline zwróci null to przekaż "0"
-
+                                               // jeśli readline zwróci null to przekaż "0"
+Console.WriteLine("Ile kolejnych liczb chcesz otrzymać?");
+int ileLiczb = int.Parse(Console.ReadLine() ?? "1"); //domyśnie 1
 
 int iloscLiczb = 0; //licznik znalezionych liczb
-while (iloscLiczb < 10) //wykonuj dopóki ilość liczb nie osiągnie 10
+
+//przygotuj tablice
+int[] tablicaLiczb = new int [ileLiczb];
+while (iloscLiczb < ileLiczb) //wykonuj dopóki ilość liczb nie osiągnie 10
 {
     liczba++; //przejdz do następnej liczby
     if (liczba % 2 == 0) //liczba jest parzysta
     {
-        Console.WriteLine(liczba.ToString()); //wypisz na ekranie
+        //Console.WriteLine(liczba.ToString()); //wypisz na ekranie
+        tablicaLiczb[iloscLiczb] = liczba; //zapisz liczbę do tablicy na pozycji określonej przez
+                                            //ilość liczb (0, 1, 2...)
         iloscLiczb++; //zwiększ licznik
     }
 }
+string znalezioneLiczby = string.Join(", ", tablicaLiczb); // połącz elementy tablicy jako string,
+                                                           // rozdzielając przecinkami 
+Console.WriteLine("Liczby: " + znalezioneLiczby);
