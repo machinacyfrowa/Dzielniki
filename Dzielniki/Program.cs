d08686aa77a6c28bec347aae85d522867edda791
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-04-08 12:28:03 +0200

    menu

--- Dzielniki/Program.cs
+++ Dzielniki/Program.cs
@@ -1,29 +1,50 @@
-﻿int liczba = 0; //liczba początkowa
-Console.WriteLine("Podaj początkową liczbę: "); //zapytaj użytkownika o liczbę
-
-liczba = int.Parse(Console.ReadLine() ?? "0"); //przeczytaj liczbę od użytkownika
-                                               // jeśli readline zwróci null to przekaż "0"
-Console.WriteLine("Jaką liczbę wykorzystać jako dzielnik:");
-int dzielnik = int.Parse(Console.ReadLine() ?? "1");//pobierz dzielnik od użytkownika
-
-Console.WriteLine("Ile kolejnych liczb chcesz otrzymać?");
-int ileLiczb = int.Parse(Console.ReadLine() ?? "1"); //domyśnie 1
-
-int iloscLiczb = 0; //licznik znalezionych liczb
-
-//przygotuj tablice
-int[] tablicaLiczb = new int [ileLiczb];
-while (iloscLiczb < ileLiczb) //wykonuj dopóki ilość liczb nie osiągnie 10
+﻿void dzielniki()
 {
-    liczba++; //przejdz do następnej liczby
-    if (liczba % dzielnik == 0) //liczba jest parzysta
+    int liczba = 0; //liczba początkowa
+    Console.WriteLine("Podaj początkową liczbę: "); //zapytaj użytkownika o liczbę
+
+    liczba = int.Parse(Console.ReadLine() ?? "0"); //przeczytaj liczbę od użytkownika
+                                                   // jeśli readline zwróci null to przekaż "0"
+    Console.WriteLine("Jaką liczbę wykorzystać jako dzielnik:");
+    int dzielnik = int.Parse(Console.ReadLine() ?? "1");//pobierz dzielnik od użytkownika
+
+    Console.WriteLine("Ile kolejnych liczb chcesz otrzymać?");
+    int ileLiczb = int.Parse(Console.ReadLine() ?? "1"); //domyśnie 1
+
+    int iloscLiczb = 0; //licznik znalezionych liczb
+
+    //przygotuj tablice
+    int[] tablicaLiczb = new int[ileLiczb];
+    while (iloscLiczb < ileLiczb) //wykonuj dopóki ilość liczb nie osiągnie 10
     {
-        //Console.WriteLine(liczba.ToString()); //wypisz na ekranie
-        tablicaLiczb[iloscLiczb] = liczba; //zapisz liczbę do tablicy na pozycji określonej przez
-                                            //ilość liczb (0, 1, 2...)
-        iloscLiczb++; //zwiększ licznik
+        liczba++; //przejdz do następnej liczby
+        if (liczba % dzielnik == 0) //liczba jest parzysta
+        {
+            //Console.WriteLine(liczba.ToString()); //wypisz na ekranie
+            tablicaLiczb[iloscLiczb] = liczba; //zapisz liczbę do tablicy na pozycji określonej przez
+                                               //ilość liczb (0, 1, 2...)
+            iloscLiczb++; //zwiększ licznik
+        }
     }
+    string znalezioneLiczby = string.Join(", ", tablicaLiczb); // połącz elementy tablicy jako string,
+                                                               // rozdzielając przecinkami 
+    Console.WriteLine("Liczby: " + znalezioneLiczby);
 }
-string znalezioneLiczby = string.Join(", ", tablicaLiczb); // połącz elementy tablicy jako string,
-                                                           // rozdzielając przecinkami 
-Console.WriteLine("Liczby: " + znalezioneLiczby);
+
+Console.WriteLine("Co chcesz zrobić:");
+Console.WriteLine("1. Pokaż kolejne liczby podzielne przez");
+Console.WriteLine("0. Wyjście");
+int wybor = int.Parse(Console.ReadLine() ?? "0");
+switch(wybor)
+{
+    case 1:
+        dzielniki();
+        break;
+    case 0:
+        Console.WriteLine("Zamykam program...");
+        break;
+    default: 
+        Console.WriteLine("Podano nieprawidłowy wybór!");
+        break;
+}
+
