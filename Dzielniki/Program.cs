27e8ea3173e8d20a19716c6697afdd8aaaec28e8
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-04-08 12:06:09 +0200

    parzyste od dowolnej liczby

--- Dzielniki/Program.cs
+++ Dzielniki/Program.cs
@@ -1,4 +1,10 @@
 ﻿int liczba = 0; //liczba początkowa
+Console.WriteLine("Podaj początkową liczbę: "); //zapytaj użytkownika o liczbę
+
+liczba = int.Parse(Console.ReadLine() ?? "0"); //przeczytaj liczbę od użytkownika
+                                                // jeśli readline zwróci null to przekaż "0"
+
+
 int iloscLiczb = 0; //licznik znalezionych liczb
 while (iloscLiczb < 10) //wykonuj dopóki ilość liczb nie osiągnie 10
 {
