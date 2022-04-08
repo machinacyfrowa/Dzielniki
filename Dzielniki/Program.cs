fb37132ccc06b82fb89f2ad546064b40f87935a3
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-04-08 12:02:10 +0200

    liczby parzyste zaczynajac od zera

--- Dzielniki/Program.cs
+++ Dzielniki/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int liczba = 0; //liczba początkowa
+int iloscLiczb = 0; //licznik znalezionych liczb
+while (iloscLiczb < 10) //wykonuj dopóki ilość liczb nie osiągnie 10
+{
+    liczba++; //przejdz do następnej liczby
+    if (liczba % 2 == 0) //liczba jest parzysta
+    {
+        Console.WriteLine(liczba.ToString()); //wypisz na ekranie
+        iloscLiczb++; //zwiększ licznik
+    }
+}
